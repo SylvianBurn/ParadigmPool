dbeaad5dbba128caa337580c1fcaf2e8e751a4e9
Sylvian <sylvian.burn@epitech.eu>
2022-01-05 10:17:01 +0100

    [ADD] Tasks 1 to 10

---
+++ My.hs
@@ -0,0 +1,38 @@
+
+mySucc :: Int -> Int
+mySucc x = x + 1
+
+myIsNeg :: Int -> Bool
+myIsNeg x
+    | x < 0 = True
+    | otherwise = False
+
+myAbs :: Int -> Int
+myAbs x
+    | x < 0 = -x
+    | otherwise = x
+
+myMin :: Int -> Int -> Int
+myMin x y
+    | x < y = x
+    | otherwise = y
+
+myMax :: Int -> Int -> Int
+myMax x y
+    | x > y = x
+    | otherwise = y
+
+myTuple :: a -> b -> (a, b)
+myTuple a b = (a, b)
+
+myTruple :: a -> b -> c -> (a, b, c)
+myTruple a b c = (a, b, c)
+
+myFst :: (a, b) -> a
+myFst (a, b) = a
+
+mySnd :: (a, b) -> b
+mySnd (a, b) = b
+
+mySwap :: (a, b) -> (b, a)
+mySwap (a, b) = (b, a)
